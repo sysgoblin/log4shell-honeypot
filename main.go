a3fad3b232e082ea18be92b1f2b09f6686757d49
Chris Bayliss <sudo@rm-rf.lol>
2021-12-15 19:31:46 +0000

    feat: service responses (#2)

--- main.go
+++ main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"flag"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httputil"
+	"net/url"
 	"regexp"
+	"strings"
 
 	"github.com/sysgoblin/log4shell-honeypot/extractor"
+	"github.com/sysgoblin/log4shell-honeypot/responses"
+)
+
+var (
+	service        *string
+	serviceOptions = []string{"elastic", "apache"}
 )
 
 func Analyse(text string, request *http.Request) {
@@ -21,22 +30,30 @@ func Analyse(text string, request *http.Request) {
 		files, err := extractor.FetchFromLdap(url)
 		if err != nil {
 			log.Printf("Failed to fetch class from %s", url)
-			continue
-		}
-
-		for _, filename := range files {
-			log.Printf("Saved payload from %s to file %s\n", request.RemoteAddr, filename)
-
-			// log rull request
-			reqDump, err := httputil.DumpRequest(request, true)
-			if err != nil {
-				log.Printf("Failed to dump request: %v", err)
-				continue
-			} else {
-				log.Printf("Request: %s", reqDump)
+		} else {
+			for _, filename := range files {
+				log.Printf("Saved payload from %s to file %s\n", request.RemoteAddr, filename)
 			}
 		}
+
+		// log rull request
+		reqDump, err := httputil.DumpRequest(request, true)
+		if err != nil {
+			log.Printf("Failed to dump request: %v", err)
+			continue
+		} else {
+			log.Printf("Request: %s", reqDump)
+		}
 	}
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
 
 func handler(w http.ResponseWriter, req *http.Request) {
@@ -50,13 +67,55 @@ func handler(w http.ResponseWriter, req *http.Request) {
 		}
 	}
 
-	w.WriteHeader(http.StatusOK)
+	// if request is get method
+	if req.Method == "GET" {
+		// check if get contains query parameters
+		if len(req.URL.Query()) > 0 {
+			// unescape query and send all parameters to Analyse
+			for _, value := range req.URL.Query() {
+				for _, v := range value {
+					ve, _ := url.QueryUnescape(v)
+					Analyse(ve, req)
+				}
+			}
+		}
+		// generate response
+		switch *service {
+		case "elastic":
+			responses.CreateElasticResponse(w)
+		case "apache":
+			responses.CreateApacheResponse(w)
+		}
+	} else if req.Method == "POST" {
+		switch *service {
+		case "apache":
+			b, _ := ioutil.ReadAll(req.Body)
+			// remove url encoding
+			body := string(b)
+			bodyEscaped, _ := url.QueryUnescape(body)
+			// add body back to request as we've consumed it
+			req.Body = ioutil.NopCloser(strings.NewReader(bodyEscaped))
+			Analyse(bodyEscaped, req)
+		default:
+			log.Printf("Service %s does not support POST method", *service)
+		}
+	}
 }
 
 func main() {
 	address := flag.String("h", ":8080", "HTTP server binding address")
+	service = flag.String("s", "apache", "Service to emulate response headers for")
 	flag.Parse()
 
+	// check if service is in serviceOptions
+	if !contains(serviceOptions, *service) {
+		log.Fatalf("Service %s is not supported", *service)
+	}
+
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(*address, nil)
+
+	err := http.ListenAndServe(*address, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
